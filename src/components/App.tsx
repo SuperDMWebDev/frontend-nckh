18f753ec3cd6682120f72634aeb71f29793fdf6b
SuperDMWebDev <minhduypham@kms-technology.com>
2023-01-04 23:29:58 +0700

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './Home';
+import NoMatch from './NoMatch';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
