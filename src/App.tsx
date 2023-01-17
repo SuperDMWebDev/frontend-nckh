ce33dfae1727a93e1e9cc021a3cd77bbaa48a020
SuperDMWebDev <hadtnt71@gmail.com>
2023-01-17 16:07:28 +0700

    jira-45: sign in

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
+import SignIn from './pages/SignIn';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NoMatch />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
