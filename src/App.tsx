f6e8019bea92dfee1857a6f9b4580b2be9997164
ddang2 <ddang2@bluecatnetworks.com>
2023-03-01 14:23:16 +0700

    add detail page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import './all.scss';
 import Admin from './pages/Admin';
 import AdminHomepage from './pages/AdminHomepage';
+import DetailPage from './pages/DetailPage';
 
 const App = () => {
   return (
@@ -16,9 +17,10 @@ const App = () => {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/home-page" element={<AdminHomepage />} />
+        <Route path="/detail-page" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  ); 
 };
 
 export default App;
