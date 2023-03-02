482ee0f37ac46df2c24d4c4475c14ffb69cd98f7
Trịnh Quyền Đế <64626305+CadenAngelos@users.noreply.github.com>
2023-03-02 16:28:15 +0700

    Jira 65 detail admin page (#11)
    * jira-66: add sidebar
    
    * add detail page
    
    ---------
    
    Co-authored-by: SuperDMWebDev <hadtnt71@gmail.com>
    Co-authored-by: ddang2 <ddang2@bluecatnetworks.com>

--- src/pages/DetailPage/style.tsx
+++ src/pages/DetailPage/style.tsx
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
+
 const Styled = styled.div`
+  .title_wrapper {
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+    padding: 15px;
+  }
+
   .title {
-    font-size: 24px;
-    margin-left: 20px;
-    margin-bottom: 40px;
+    width: 169px;
+    height: 33px;
+    left: 438px;
+    top: 136px;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 33px;
+    /* identical to box height */
+
+    letter-spacing: -0.01em;
+
+    color: #1677ff;
+
+    text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
   }
 
   .content-wrapper {
@@ -15,11 +37,11 @@ const Styled = styled.div`
 
   .content_row {
     display: flex;
-    width: 40%;
     align-items: center;
+    width: 60%;
     justify-content: space-between;
-    gap: 30px;
     flex-direction: row;
+    margin-bottom: 20px;
   }
 
   .row_title {
