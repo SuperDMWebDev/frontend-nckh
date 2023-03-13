664152ae5729ef2d87b4a810d029e48b5ced4fa4
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2023-03-13 21:23:16 +0700

    update fe list article (#15)
    resolve conflict merge branch jira-64-list-screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Admin from './pages/Admin';
 import AdminHomepage from './pages/AdminHomepage';
 import DetailPage from './pages/DetailPage';
 import ModalTeacher from './components/AdminList/ModalTeacher';
+import Loader from './components/Loader/Loader';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
@@ -20,7 +21,7 @@ const App = () => {
         <Route path="/admin" element={<Admin />} />
         <Route path="/home-page" element={<AdminHomepage />} />
         <Route path="/detail-page" element={<DetailPage />} />
-        <Route path="/test" element={<ModalTeacher />} />
+        <Route path="/loader" element={<Loader />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/AdminList/ListTeacher/index.tsx
+++ src/components/AdminList/ListTeacher/index.tsx
@@ -1,11 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { SearchOutlined, MoreOutlined, DeleteOutlined } from '@ant-design/icons';
 import type { InputRef } from 'antd';
 import { Button, Input, Space, Table } from 'antd';
 import type { ColumnsType, ColumnType } from 'antd/es/table';
 import type { FilterConfirmProps } from 'antd/es/table/interface';
 import type { MenuProps } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import { Dropdown } from 'antd';
+import { Modal } from 'antd';
+import ModalTeacher from '../ModalTeacher';
+import Loader from '../../Loader/Loader';
 import './style.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -46,10 +50,20 @@ const items: MenuProps['items'] = [
 ];
 
 const ListTeacher: React.FC = () => {
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
-  const navigate = useNavigate();
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef<InputRef>(null);
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1500);
+    }, [])
+  
+
 
   const handleSearch = (
     selectedKeys: string[],
@@ -188,14 +202,36 @@ const ListTeacher: React.FC = () => {
     }
   ];
 
-  return (
-    <Table
-      pagination={{ pageSize: 7 }}
-      columns={columns}
-      dataSource={data}
-      rowClassName={(record, index) => (index % 2 === 0 ? 'table-row-light' : 'table-row-dark')}
-    />
-  );
+    return <>
+        {
+            loading ? <Loader /> : <div>
+                <div className='header_table'>
+                    <span className='title_table'>List of Teachers</span>
+                    <button className='button2' onClick={() => setOpen(true)}><PlusOutlined style={{ marginRight: "10px" }} />Add</button>
+                </div>
+
+                <Table
+                    pagination={{ pageSize: 7 }}
+                    columns={columns}
+                    dataSource={data}
+                    rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' : 'table-row-dark'}
+                />;
+
+                <Modal
+                    className='title_modal'
+                    title="Add Teacher"
+                    centered
+                    open={open}
+                    onOk={() => setOpen(false)}
+                    onCancel={() => setOpen(false)}
+                    width={800}
+                >
+                    <ModalTeacher />
+                </Modal>
+            </div>
+        }
+    </>
+
 };
 
 export default ListTeacher;
