b7fc47f4696884fb2ce0898d396d17aa5dfc8ce7
ddang2 <ddang2@bluecatnetworks.com>
2023-04-26 18:09:39 +0700

    finist 2

--- src/components/User/ModalArticle/ModalArticle.tsx
+++ src/components/User/ModalArticle/ModalArticle.tsx
@@ -2,8 +2,6 @@ import React, { useState, useRef } from 'react';
 import { DatePicker, Form, Input } from 'antd';
 import Styled from './style';
 import InputTags from '../InputTags/InputTags';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
 
 type SizeType = Parameters<typeof Form>[0]['size'];
 
