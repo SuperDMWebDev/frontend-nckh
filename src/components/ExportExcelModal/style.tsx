100df3543a04f6c462dd1e8dbe1ff8e38efcd265
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2023-06-18 10:29:44 +0700

    export exce; (#100)
    Co-authored-by: minhduypham <minhduypham@kms-technology.com>

---
+++ src/components/ExportExcelModal/style.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledExportExcelModal = styled.div`
+  .year-select {
+    width: 100%;
+  }
+
+  .year-select .ant-select-selector {
+    border-radius: 4px;
+  }
+
+  .year-select .ant-select-selection-placeholder {
+    color: #bfbfbf;
+  }
+
+  .year-select .ant-select-selection-item {
+    padding: 5px 8px;
+  }
+`;
+
+export default StyledExportExcelModal;
